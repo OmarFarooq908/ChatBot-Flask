6cbff628376362242305ae8f200fa5787a0bada1
omarfarooq908 <mohamed.omar.farooq@gmail.com>
2023-09-14 19:42:25 +0500

    Added default response and similarity threshold

--- index.py
+++ index.py
@@ -5,7 +5,13 @@ from chatterbot.trainers import ListTrainer
 #3rd param = gives the chatbot build its logic upon
 
 #chatterbot.logic.BestMatch --> Respond with simple chat strings
-omar_bot = ChatBot("Omar-Bot", read_only=False, logic_adapters=["chatterbot.logic.BestMatch"])
+omar_bot = ChatBot("Omar-Bot", read_only=False, logic_adapters=[
+    {
+        "import_path":"chatterbot.logic.BestMatch",
+        "default_response":"I don't have answer to this. Ask something else.",
+        "maximum_similarity_threshold":0.9 #If similarity < 0.9, return default response
+    }
+    ])
 
 #Training list
 #First element is a question, successive element is answer
@@ -16,8 +22,22 @@ training_list = [
     "What's your name?",
     "I'm a Omar-Bot",
     "How old are you?",
-    "I was born on Sept 14, 2023"
+    "I was born on Sept 14, 2023",
+    "Are you mad?",
+    "No! I'm not",
+    "Do you have Android?",
+    "Haha, I have everything :)",
+    "What's your favorite food?",
+    "I don't eat",
+    "What's your job?",
+    "I'm here to answer your questions"
 ]
 #Train the chatbot
 list_trainer = ListTrainer(omar_bot)
 list_trainer.train(training_list)
+
+#Prompt the user for input
+while True:
+    user_input = input("Human: ")
+    #Tells the chatbot to pick out appropriate response to the input
+    print("Chatterbot: " + str(omar_bot.get_response(user_input)))
